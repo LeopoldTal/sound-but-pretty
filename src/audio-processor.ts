c0d8ece23038a03576dd000fd7595f664cb8625a
Leopold Tal G <leopold.tal.dg@gmail.com>
2021-11-08 22:55:19 +0100

    show volume and frequency

--- src/audio-processor.ts
+++ src/audio-processor.ts
@@ -1,28 +1,50 @@
+type VolumeSnapshot = {
+	meanVolume: number;
+	volumesByFreq: Uint8Array;
+};
+
+// There's always noise that's not very important to human ears
+// Analyse volume only in this range
+const VOLUME_MIN_FREQ = 500; // Hz
+const VOLUME_MAX_FREQ = 15000; // Hz
+
 export class AudioProcessor {
 	private source: AudioNode;
-	private nbBins: number;
 	private analyser: AnalyserNode;
 	private playbackEnabled: boolean;
 
 	constructor(source: AudioNode, nbBins: number, playbackEnabled = true) {
 		this.source = source;
-		this.nbBins = nbBins;
 		this.playbackEnabled = playbackEnabled;
 
 		this.analyser = this.source.context.createAnalyser();
-		this.analyser.fftSize = 2 * this.nbBins;
+		this.analyser.fftSize = 2 * nbBins;
 		if (this.playbackEnabled) {
 			this.analyser.connect(this.source.context.destination);
 		}
 		this.source.connect(this.analyser);
 	}
 
-	getNbBins(): number { return this.nbBins; }
+	getMaxFreq(): number { return this.source.context.sampleRate / 2; }
+	getNbBins(): number { return this.analyser.frequencyBinCount; }
 
-	getFrequencies(): Uint8Array {
-		const dataArray = new Uint8Array(this.analyser.frequencyBinCount);
+	getFrequencies(): VolumeSnapshot {
+		const nbBins = this.getNbBins();
+
+		const dataArray = new Uint8Array(nbBins);
 		this.analyser.getByteFrequencyData(dataArray);
-		return dataArray;
+		const volumesByFreq = dataArray.map(volume => volume * 100 / 255);
+
+		const maxFreq = this.getMaxFreq();
+		const minBin = Math.floor(VOLUME_MIN_FREQ / maxFreq * nbBins);
+		const maxBin = Math.ceil(VOLUME_MAX_FREQ / maxFreq * nbBins);
+		const binsForVolume = volumesByFreq.slice(minBin, maxBin);
+		const meanVolume = binsForVolume.reduce((a, b) => a + b, 0) / binsForVolume.length;
+
+		return {
+			volumesByFreq,
+			meanVolume
+		};
 	}
 
 	isPlaybackEnabled() {
--- src/audio-renderer.ts
+++ src/audio-renderer.ts
@@ -3,6 +3,7 @@ import { AudioProcessor } from './audio-processor';
 export class AudioRenderer {
 	private processor: AudioProcessor;
 	private bars: HTMLElement[] = [];
+	private meanVolumeLevel: HTMLElement | null = null;
 	private interval: number | null = null;
 
 	constructor(processor: AudioProcessor, renderTarget: HTMLElement) {
@@ -22,9 +23,12 @@ export class AudioRenderer {
 
 	private renderOnce() {
 		const nbBins = this.processor.getNbBins();
-		const volumes = this.processor.getFrequencies();
+		const { meanVolume, volumesByFreq } = this.processor.getFrequencies();
 		for (let ii = 0; ii < nbBins; ii++) {
-			this.bars[ii].style.height = `${volumes[ii] * 100 / 255}%`;
+			this.bars[ii].style.height = `${volumesByFreq[ii]}%`;
+		}
+		if (this.meanVolumeLevel) {
+			this.meanVolumeLevel.style.bottom = `${meanVolume}%`;
 		}
 	}
 
@@ -37,6 +41,11 @@ export class AudioRenderer {
 		this.bars = [];
 		const barsContainer = document.createElement('div');
 		barsContainer.className = 'bars-container';
+		barsContainer.addEventListener('mousemove', (e) => {
+			const { x, width } = barsContainer.getBoundingClientRect();
+			const freq = this.processor.getMaxFreq() * (e.clientX - x) / width;
+			barsContainer.title = `${Math.round(freq)} Hz`;
+		});
 
 		const nbBins = this.processor.getNbBins();
 		const barWidth = 100 / nbBins;
@@ -52,6 +61,11 @@ export class AudioRenderer {
 			barsContainer.appendChild(bar);
 			this.bars.push(bar);
 		}
+
+		this.meanVolumeLevel = document.createElement('div');
+		this.meanVolumeLevel.className = 'mean-volume-level';
+		this.meanVolumeLevel.style.bottom = '50%';
+		barsContainer.appendChild(this.meanVolumeLevel);
 
 		renderTarget.appendChild(barsContainer);
 	}
